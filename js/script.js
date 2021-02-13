952f45c2c93bf489e50dda75f9609ae0a750a3a9
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-02-13 17:12:54 +0100

    Fix bug in article loop

--- js/script.js
+++ js/script.js
@@ -71,29 +71,30 @@
 
       const articleId = article.getAttribute("id");
       console.log("id article: ", articleId);
+
+      /* find the title element */
+
+      /* [DONE] get the title from the title element */
+
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      console.log("title article: ", articleTitle);
+
+      /* create HTML of the link */
+
+      const linkHTML =
+        '<li><a href="#' +
+        articleId +
+        '"><span>' +
+        articleTitle +
+        "</span></a></li>";
+      console.log(linkHTML);
+
+      /* insert link into titleList */
+
+      html = html + linkHTML;
+      console.log("variables: ", html);
     }
 
-    /* find the title element */
-
-    /* [DONE] get the title from the title element */
-
-    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-    console.log("title article: ", articleTitle);
-
-    /* create HTML of the link */
-
-    const linkHTML =
-      '<li><a href="#' +
-      articleId +
-      '"><span>' +
-      articleTitle +
-      "</span></a></li>";
-    console.log(linkHTML);
-
-    /* insert link into titleList */
-
-    html = html + linkHTML;
-    console.log("variables: ", html);
     titleList.innerHTML = html;
 
     const links = document.querySelectorAll(".titles a");
