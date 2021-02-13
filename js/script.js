87efaf879355a68cacd0d50f076b4322bb841322
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-02-13 16:34:01 +0100

    Add loop for article and link for title

--- js/script.js
+++ js/script.js
@@ -50,28 +50,58 @@
     targetArticle.classList.add("active");
   };
 
-  const links = document.querySelectorAll(".titles a");
-
-  for (let link of links) {
-    link.addEventListener("click", titleClickHandler);
-  }
-
   const optArticleSelector = ".post",
     optTitleSelector = ".post-title",
     optTitleListSelector = ".titles";
 
   function generateTitleLinks() {
-    /* remove contents of titleList */
+    /* [DONE] remove contents of titleList */
 
     const titleList = document.querySelector(optTitleListSelector);
     titleList.innerHTML = "";
 
-    /* for each article */
-    /* get the article id */
+    /* [DONE] for each article */
+
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = "";
+
+    for (let article of articles) {
+      /* [DONE] get the article id */
+
+      const articleId = article.getAttribute("id");
+      console.log("id article: ", articleId);
+    }
+
     /* find the title element */
-    /* get the title from the title element */
+
+    /* [DONE] get the title from the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log("title article: ", articleTitle);
+
     /* create HTML of the link */
+
+    const linkHTML =
+      '<li><a href="#' +
+      articleId +
+      '"><span>' +
+      articleTitle +
+      "</span></a></li>";
+    console.log(linkHTML);
+
     /* insert link into titleList */
+
+    html = html + linkHTML;
+    console.log("variables: ", html);
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll(".titles a");
+    console.log(links);
+
+    for (let link of links) {
+      link.addEventListener("click", titleClickHandler);
+    }
   }
 
   generateTitleLinks();
